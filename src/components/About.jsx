bfeb57b2fd2a8f38fd34951c1e42de29d980ab30
Aarjav shukla <aarjavshukla38@gmail.com>
2026-04-23 19:11:37 +0530

    commit

---
+++ src/components/About.jsx
@@ -0,0 +1,86 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+
+const About = () => {
+  useGSAP(() => {
+    gsap.from(".content .left", {
+      opacity: 0,
+      yPercent: 10,
+      ease: "expo.inOut",
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top 50%",
+      },
+    });
+    gsap.from(".content .right", {
+      opacity: 0,
+      yPercent: 10,
+      ease: "expo.inOut",
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top 50%",
+      },
+    });
+    gsap.from(".top-grid", {
+      opacity: 0,
+      yPercent: 10,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top 20%",
+      },
+    });
+    gsap.from(".bottom-grid", {
+      opacity: 0,
+      yPercent: 10,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top 20%",
+      },
+    });
+  }, []);
+  return (
+    <div id="about">
+      <div className="content">
+        <div className="left">
+          <button className="badge">Best Cocktails</button>
+          <h2>Where every detail matters-from muddle to garnish</h2>
+        </div>
+        <div className="right sub-content">
+          <p>
+            Every cocktail we serve is a reflection of our obsession with detail
+            — from the first muddle to the final garnish. That care is what
+            turns a simple drink into something truly memorable.
+          </p>
+          <div>
+            <h3 className="text-3xl font-extrabold">
+              <span>4.5</span>/5
+            </h3>
+            <p className="text-sm">More than +12000 customers</p>
+          </div>
+        </div>
+      </div>
+      <div className="top-grid">
+        <div className="col-span-3">
+          <img src="images/abt1.png" alt="" />
+        </div>
+        <div className="col-span-6">
+          <img src="images/abt2.png" alt="" />
+        </div>
+        <div className="col-span-3">
+          <img src="images/abt5.png" alt="" />
+        </div>
+      </div>
+      <div className="bottom-grid">
+        <div className="col-span-8">
+          <img src="images/abt3.png" alt="" />
+        </div>
+        <div className="col-span-4">
+          <img src="images/abt4.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,116 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
+
+const Hero = () => {
+  const videoRef = useRef();
+  let isMobile = useMediaQuery({maxWidth : 767})
+  useGSAP(() => {
+    const loaderTimeline = gsap.timeline();
+    const headingText = new SplitText("#hero > h1", { type: "chars, words" });
+    const paragraphText = new SplitText(".body p", { type: "lines" });
+    headingText.chars.forEach((char) => char.classList.add("text-gradient"));
+    loaderTimeline
+      .from(headingText.chars, {
+        yPercent: 80,
+        duration: 0.6,
+        stagger: 0.02,
+      })
+      .from(paragraphText.lines, {
+        opacity: 0,
+        yPercent: 80,
+        stagger: 0.06,
+        duration: 0.8,
+      });
+  }, []);
+  useGSAP(() => {
+    gsap.to(".left-leaf", {
+      yPercent: -40,
+      scrollTrigger: {
+        trigger: "#hero",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(
+      ".right-leaf",
+      {
+        yPercent: 40,
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "bottom bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      },
+      [],
+    );
+    const startValue = isMobile ? "top 40%" : "top 20%";
+    const endValue = isMobile ? "120% top" : "bottom 11%";
+    const video = videoRef.current;
+    const videoTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: video,
+        start: startValue,
+        end: endValue,
+        // start: "top 20%",
+        // end: "bottom top",
+        scrub: true,
+        pin: true,
+      },
+    });
+    video.addEventListener("loadedmetadata", () => {
+      videoTl.to(video, {
+        currentTime: video.duration,
+        ease: "none",
+      });
+    });
+  });
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1>mojito</h1>
+        <img
+          src="images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+        <div className="body">
+          <div className="content">
+            <div className="sipThe">
+              <p>Cool. Crisp. Classic.</p>
+              <p>Sip the Spirit of Summer</p>
+            </div>
+            <div className="view-cocktails">
+              <p>
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">view cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="video absolute inset-0 no-scroll">
+        <video
+          ref={videoRef}
+          src="videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
